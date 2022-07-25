2786577db93d1de6e6cb1e9831f2a609e1687287
zihur <dog60508@gmail.com>
2022-07-25 13:16:48 +0800

    add base config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/credit-cards-layout/" // note the trailing slash
+      : "/",
+});
