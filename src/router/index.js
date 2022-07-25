7b3263393e6ee70e1f004ba2ac4e7c37381676cc
zihur <dog60508@gmail.com>
2022-07-25 12:34:26 +0800

    show main with router

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+const routes = [
+  { path: "/", name: "home", component: () => import("@/views/ShowMain.vue") },
+  { path: "/second", name: "second", component: () => import("@/views/ShowSecond.vue") },
+  { path: "/third", name: "third", component: () => import("@/views/ShowThird.vue") },
+  { path: "/fourth", name: "fourth", component: () => import("@/views/ShowFourth.vue") },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: () => import("@/views/NotFound.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  linkActiveClass: "router-link-active", // default link active class name
+});
+
+router.beforeEach((to, from) => {
+  if (to.meta.requiresAuth && !window.user) {
+    return { name: "login", query: to.query };
+  }
+});
+
+export default router;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,6 @@
+<srcipt setup>
+</srcipt>
+<template>
+  <div>Not Found</div>
+  <p>沒有此網頁唷~</p>
+</template>
