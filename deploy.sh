c6d91c7b5b91d45b91d0faef0144f2ecafadb64e
zihur <dog60508@gmail.com>
2022-07-25 13:24:26 +0800

    fix endpoint

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zihur/credit-cards.git main:gh-pages
+git push -f git@github.com:zihur/credit-cards-layout.git main:gh-pages
 
 cd -
